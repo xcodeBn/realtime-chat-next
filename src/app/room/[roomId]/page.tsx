05f3f1779b5050380bb3774bd2b89ab0e8f62564
XcodeBn <86073708+xcodeBn@users.noreply.github.com>
2026-01-22 09:19:57 +0200

    feat: implement chat input and message sending functionality on room page
    Signed-off-by: XcodeBn <86073708+xcodeBn@users.noreply.github.com>

--- src/app/room/[roomId]/page.tsx
+++ src/app/room/[roomId]/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import {useParams} from "next/navigation";
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 
 function formatTimeRemaining(seconds: number) {
     const mins = Math.floor(seconds / 60);
@@ -13,7 +13,9 @@ const Page = () => {
     const params = useParams();
     const roomId = params.roomId as string;
     const [copyStatus, setCopyStatus] = useState<"copy" | "copied!">("copy");
-    const [timeRemaining, setTimeRemaining] = useState<number | null>(2423);
+    const [timeRemaining, setTimeRemaining] = useState<number | null>(null);
+    const [input,setInput] = useState("");
+    const inputRef = useRef<HTMLInputElement>(null);
 
 
     const  copyLink = () => {
@@ -62,6 +64,37 @@ const Page = () => {
                     DESTROY NOW
                 </button>
             </header>
+            <div className={"flex-1 overflow-y-auto p-4 space-y-4 scrollbar-thin"}>
+                {/* Chat messages will go here */}
+            </div>
+            <div className={"p-4 border-t border-zinc-800 bg-zinc-900/30"}>
+                <div className={"flex flex-row"}>
+
+                <div className={" flex-1 relative group"}>
+                    <span className={"absolute left-4 top-1/2 -translate-y-1/2 text-green-500 animate-pulse"}>
+                        {">"}
+                    </span>
+                    <input
+                        value={input}
+                        onChange={(e)=>{setInput(e.target.value)}}
+                        onKeyDown={(e)=>{
+                            if(e.key==="Enter" && input.trim()){
+                                inputRef.current?.focus();
+                                setInput("");
+                            }
+                        }}
+                        placeholder={"Type message..."}
+                        autoFocus={true}
+                        className={"w-full bg-black border border-zinc-800 focus:border-zinc-700 focus:outline-none transition-colors text-zinc-100 placeholder:text-zinc-700 py-3 pl-8 pr-4 text-sm"}
+                    />
+
+                </div>
+                <button className={"bg-zinc-800 text-zinc-400 px-6 text-sm font-bold hover:text-zinc-200 transition-all disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer"}>
+                    Send
+                </button>
+                </div>
+
+            </div>
         </main>
 
     )
