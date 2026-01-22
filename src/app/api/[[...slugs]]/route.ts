2c24a748fdde5b51dbb46fc761f04837628b703e
XcodeBn <86073708+xcodebn@users.noreply.github.com>
2026-01-22 11:15:31 +0200

    feat: add message handling with authentication and validation for messages route
    Signed-off-by: XcodeBn <86073708+xcodeBn@users.noreply.github.com>

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -2,6 +2,9 @@
 import { Elysia, t } from 'elysia'
 import {nanoid} from "nanoid";
 import {redis} from "@/lib/redis";
+import {authMiddleware} from "@/app/api/[[...slugs]]/auth";
+import {z} from "zod";
+import {awaitExpression} from "@babel/types";
 
 const ROOM_TTL_SECONDS = 60 * 10 // 1 hour
 
@@ -17,7 +20,29 @@ const rooms = new Elysia({prefix: "/room"})
 
         return {roomId}
     })
-const app = new Elysia({ prefix: "/api" }).use(rooms)
+
+const messages = new Elysia({prefix:"/messages"}).use(authMiddleware).post("/", async ({body,auth})=>{
+    const {sender,text} = body
+    const roomExists = await redis.exists
+    (`meta:${auth.roomId}`)
+    if(!roomExists){
+        throw new Error("Room does not exist")
+    }
+
+},{
+    query: z.object({
+        roomId: z.string()
+    }),
+    body: z.object({
+        sender: z.string().max(100),
+        text: z.string().max(1000)
+    })
+})
+
+
+
+
+const app = new Elysia({ prefix: "/api" }).use(rooms).use(messages)
 
 export const GET = app.fetch
 export const POST = app.fetch
