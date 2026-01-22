72d82c01d10fdaab3a17c4764249bc32943e7108
XcodeBn <86073708+xcodebn@users.noreply.github.com>
2026-01-22 09:49:03 +0200

    feat: implement proxy functionality for room access and authentication
    Signed-off-by: XcodeBn <86073708+xcodeBn@users.noreply.github.com>

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -8,7 +8,7 @@ const ROOM_TTL_SECONDS = 60 * 10 // 1 hour
 const rooms = new Elysia({prefix: "/room"})
     .post("/create", async ()=>{
         const roomId = nanoid()
-        await redis.hset(`meta${roomId}`,{
+        await redis.hset(`meta:${roomId}`,{
             connected: [],
             createdAt: Date.now()
         })
---
+++ src/proxy.ts
@@ -0,0 +1,35 @@
+import {NextRequest, NextResponse} from "next/server";
+import {redis} from "@/lib/redis";
+import {nanoid} from "nanoid";
+
+export async function proxy(req:NextRequest){
+    const pathName = req.nextUrl.pathname;
+    const roomMatch = pathName.match(/^\/room\/([^\/]+)(\/.*)?$/);
+    if(!roomMatch){
+        return NextResponse.redirect(new URL("/",req.url));
+    }
+    const roomId = roomMatch[1];
+    const meta = await redis.hgetall<{connected:string[],createdAt:number}>(`meta:${roomId}`)
+
+    if(!meta){
+        return NextResponse.redirect(new URL("/?error=room-not-found",req.url));
+    }
+
+    const response = NextResponse.next();
+    const token = nanoid()
+    response.cookies.set("x-auth-token",token,{
+        path: "/",
+        httpOnly : true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict"
+    })
+
+    await redis.hset(`meta:${roomId}`,{
+        connected: [...meta.connected, token]
+    })
+    return response;
+}
+
+export const config = {
+    matcher: '/room/:path*'
+}
