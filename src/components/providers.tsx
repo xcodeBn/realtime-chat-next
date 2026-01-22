233892d115e7542cbb085b98926128f149b5a409
XcodeBn <86073708+xcodeBn@users.noreply.github.com>
2026-01-22 11:28:04 +0200

    feat: integrate Realtime functionality with message handling and routing
    Signed-off-by: XcodeBn <86073708+xcodeBn@users.noreply.github.com>

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -5,10 +5,13 @@
 import React, {useState} from "react";
 import {QueryClient} from "@tanstack/query-core";
 import {QueryClientProvider} from "@tanstack/react-query";
+import {RealtimeProvider} from "@upstash/realtime/client";
 
 export const Providers = ({children} : {children: React.ReactNode}) => {
     const [queryClient] = useState(() => new QueryClient());
-    return <QueryClientProvider client={queryClient}>
+    return <RealtimeProvider>
+        <QueryClientProvider client={queryClient}>
         {children}
     </QueryClientProvider>
+    </RealtimeProvider>
 }
